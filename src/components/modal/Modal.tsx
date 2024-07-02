acfa5626a9a89045df47493d7a4a441b8d07ff7f
Elma Narimani <narimanielma@gmail.com>
2024-07-02 17:05:49 +0330

    modal component

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,44 +1,52 @@
-import React from 'react';
-import { cva, VariantProps } from 'class-variance-authority';
-import clsx from 'clsx';
+import React from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import clsx from "clsx";
 
 const modalStyles = cva(
-    'fixed inset-0 z-50 overflow-auto flex justify-center items-center',
-    {
-        variants: {
-            transition: {
-                true: 'transition duration-300 ease-in-out'
-            },
-            fade: {
-                true: 'opacity-0'
-            },
-            sizes: {
-                sm: 'max-w-sm',
-                md: 'max-w-md',
-                lg: 'max-w-lg',
-                xl: 'max-w-xl',
-                full: 'w-full'
-            },
-            placement: {
-                top: 'items-start',
-                center: 'items-center',
-                bottom: 'items-end'
-            }
-        },
-        defaultVariants: {
-            transition: true,
-            fade: true,
-            sizes: 'sm',
-            placement: 'center'
-        },
-    }
+  "relative top-[50%] left-[50%] translate-x-[-50%] border border-gray-200 p-4 rounded rounded-md overflow-auto shadow-md z-50",
+  {
+    variants: {
+      transition: {
+        true: "transition-opacity duration-500 ease-in-out",
+      },
+      sizes: {
+        sm: "max-w-sm",
+        md: "max-w-md",
+        lg: "max-w-lg",
+        xl: "max-w-xl",
+        full: "w-full",
+      },
+      placement: {
+        top: "items-start",
+        center: "items-center",
+        bottom: "items-end",
+      },
+    },
+    defaultVariants: {
+      transition: true,
+      sizes: "sm",
+      placement: "center",
+    },
+  }
 );
 
-interface ModalProps extends React.HTMLAttributes<HTMLElement>, VariantProps<typeof modalStyles> {
+interface ModalProps
+  extends React.HTMLAttributes<HTMLElement>,
+    VariantProps<typeof modalStyles> {
+  modalOpen: boolean;
 }
 
-export const Modal: React.FC<ModalProps> = ({ className, children, transition, fade, sizes, placement, ...props }) => {
-    return (
-        <div className={clsx(modalStyles({ placement, sizes, transition, fade }), className)} {...props} />
-    );
+export const Modal: React.FC<ModalProps> = ({
+  className,
+  modalOpen,
+  placement,
+  sizes,
+  transition,
+  ...props
+}) => {
+  const classes = modalStyles({ placement, sizes, transition });
+
+  if (!modalOpen) return null;
+
+  return <div className={clsx(classes, className)} {...props} />;
 };
