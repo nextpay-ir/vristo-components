cdf8c0c99783d1d7ccaee5432a23ccf959823f32
Amirhossein Zarei <amir.zarei1381913@gmail.com>
2024-07-04 19:37:14 +0330

    Modal became more complete

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,52 +1,54 @@
-import React from "react";
+import React, { useRef } from "react";
 import { cva, VariantProps } from "class-variance-authority";
 import clsx from "clsx";
 
 const modalStyles = cva(
-  "relative top-[50%] left-[50%] translate-x-[-50%] border border-gray-200 p-4 rounded rounded-md overflow-auto shadow-md z-50",
+  "absolute w-full h-full sm:h-fit sm:w-fit top-0 sm:top-[25%] left-[50%] translate-x-[-50%] p-4 bg-white rounded-none sm:rounded-md shadow-md",
   {
     variants: {
-      transition: {
-        true: "transition-opacity duration-500 ease-in-out",
-      },
-      sizes: {
+      size: {
         sm: "max-w-sm",
         md: "max-w-md",
         lg: "max-w-lg",
         xl: "max-w-xl",
         full: "w-full",
       },
-      placement: {
-        top: "items-start",
-        center: "items-center",
-        bottom: "items-end",
-      },
     },
     defaultVariants: {
-      transition: true,
-      sizes: "sm",
-      placement: "center",
     },
   }
 );
 
 interface ModalProps
   extends React.HTMLAttributes<HTMLElement>,
-    VariantProps<typeof modalStyles> {
-  modalOpen: boolean;
+  VariantProps<typeof modalStyles> {
+  open: boolean;
+  onClose: () => void;
 }
 
 export const Modal: React.FC<ModalProps> = ({
   className,
-  modalOpen,
-  placement,
-  sizes,
-  transition,
+  open,
+  onClose,
+  size,
   ...props
 }) => {
-  const classes = modalStyles({ placement, sizes, transition });
+  const classes = modalStyles({ size });
 
-  if (!modalOpen) return null;
+  const modalRef = useRef<HTMLDivElement>(null);
 
-  return <div className={clsx(classes, className)} {...props} />;
+  const handleOverlayClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      onClose();
+    }
+  };
+
+  if (!open || !onClose) return null;
+
+  return (
+    <div className="fixed z-[1300] inset-0">
+      <div className="opacity-[1] fixed flex items-center justify-center right-0 bottom-0 top-0 left-0 z-[-1] bg-slate-500" onClick={handleOverlayClick} />
+      <div ref={modalRef} className={clsx(classes, className)} {...props} />;
+    </div>
+  )
 };
