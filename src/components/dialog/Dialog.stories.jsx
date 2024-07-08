5b578c65d28ba1a9405dc5db727ca84b2723c1bb
AmirZarei2002 <amir.zarei1381913@gmail.com>
2024-07-08 16:08:58 +0330

    Create dialog

--- src/components/dialog/Dialog.stories.jsx
+++ src/components/dialog/Dialog.stories.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Modal } from "./Modal";
+import { Modal } from "./Dialog.js";
 import { Icon } from "@iconify/react";
 
 export default {
---
+++ src/components/dialog/Dialog.tsx
@@ -0,0 +1,55 @@
+import React, {useRef} from "react";
+import {cva, VariantProps} from "class-variance-authority";
+import clsx from "clsx";
+
+const dialogStyles = cva(
+    "absolute w-full top-0 sm:top-[25%] left-[50%] translate-x-[-50%] p-4 bg-white rounded-none sm:rounded-md shadow-md",
+    {
+        variants: {
+            size: {
+                xs: "max-w-[444px]",
+                sm: "max-w-[600px]",
+                md: "max-w-[900px]",
+                lg: "max-w-[1200px]",
+                xl: "max-w-[1536px]",
+            },
+        },
+        defaultVariants: {},
+    }
+);
+
+interface DialogProps
+    extends React.HTMLAttributes<HTMLElement>,
+        VariantProps<typeof dialogStyles> {
+    open: boolean;
+    onClose: () => void;
+}
+
+export const Dialog: React.FC<DialogProps> = ({
+                                                  className,
+                                                  open,
+                                                  onClose,
+                                                  size,
+                                                  ...props
+                                              }) => {
+    const classes = dialogStyles({size});
+
+    const dialogRef = useRef<HTMLDivElement>(null);
+
+    const handleOverlayClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (dialogRef.current && !dialogRef.current.contains(event.target as Node)) {
+            onClose();
+        }
+    };
+
+    if (!open || !onClose) return null;
+
+    return (
+        <div className="fixed z-[1300] inset-0">
+            <div
+                className="opacity-[1] fixed flex items-center justify-center right-0 bottom-0 top-0 left-0 z-[-1] bg-[#00000080]"
+                onClick={handleOverlayClick}/>
+            <div ref={dialogRef} className={clsx(classes, className)} {...props} />
+        </div>
+    )
+};
