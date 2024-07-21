1389e809de1b86389ea7d9ad2099a3fbacbcb513
AmirZarei2002 <amir.zarei1381913@gmail.com>
2024-07-21 14:44:38 +0330

    feat: create skeleton component

---
+++ src/components/skeleton/Skeleton.tsx
@@ -0,0 +1,23 @@
+import {cva, VariantProps} from 'class-variance-authority';
+import clsx from 'clsx';
+import React from 'react';
+
+const skeletonStyles = cva('bg-gray-200 animate-pulse rounded');
+
+interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof skeletonStyles> {
+    height?: "custom" | string;
+    width?: "custom" | string;
+    isLoaded?: boolean;
+}
+
+export const Skeleton: React.FC<SkeletonProps> = ({className, ...props}) => {
+    const customStyles: React.CSSProperties = {};
+
+    return (
+        <div
+            className={clsx(skeletonStyles(), className)}
+            style={customStyles}
+            {...props}
+        />
+    );
+};
